ee873408e964394fdc3810fec1acdd05d6086151
RyoheiKudo <67203889+RyoheiKudo@users.noreply.github.com>
2020-06-25 02:47:06 +0900

    Add files via upload

---
+++ MyPortfolio/html/r/index1.r
@@ -0,0 +1,21 @@
+#Run Source (Ctrl+Shift+S) and Run Selected Line(Ctrl+Enter)
+#Command + Enter で実行？
+
+#X~B(N,p)、P(X=x)
+#gamma(n+1)でnの階乗(=n!)
+temp <- function(n) {gamma(n+1)}
+p <- ((temp (N))/(temp(N - x)*(temp(x)))*((p)^x)*((1-p)*(N-x)))
+p
+factor(fa
+
+afa
+fa
+fa
+f
+agfag
+a
+ga
+gf
+a
+gagag
+)
