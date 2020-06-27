c82d2a1037401e18e7651db0c3d053d8e9a3869d
RyoheiKudo <67203889+RyoheiKudo@users.noreply.github.com>
2020-06-27 11:52:48 +0900

    Add files via upload

--- MyPortfolio/html/r/index1.r
+++ MyPortfolio/html/r/index1.r
@@ -1,21 +1,14 @@
 #Run Source (Ctrl+Shift+S) and Run Selected Line(Ctrl+Enter)
 #Command + Enter で実行？
 
+#計算
+#和+ 差- 積* 商/ 冪乗^ 整数商%/% 剰余%%
+#値
+#ナル「何も示さない」
+
 #X~B(N,p)、P(X=x)
 #gamma(n+1)でnの階乗(=n!)
 temp <- function(n) {gamma(n+1)}
 p <- ((temp (N))/(temp(N - x)*(temp(x)))*((p)^x)*((1-p)*(N-x)))
 p
-factor(fa
 
-afa
-fa
-fa
-f
-agfag
-a
-ga
-gf
-a
-gagag
-)
