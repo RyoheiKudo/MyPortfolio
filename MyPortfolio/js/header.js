7971bc9ad1ba50e0e875ca028036b26968d52b3e
RyoheiKudo <67203889+RyoheiKudo@users.noreply.github.com>
2020-07-13 06:44:47 +0900

    Add files via upload

--- MyPortfolio/js/header.js
+++ MyPortfolio/js/header.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function set2fig(num) {
   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
   let ret;
@@ -16,5 +18,21 @@ function showClock2() {
   const msg =
     "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
   document.getElementById("timeClock").innerHTML = msg;
+
+  const timeComments = document.getElementById("timeComments");
+
+  let clockComments;
+
+  if (nowHour < 4) {
+    clockComments = "夜更かしですか？お仕事ですか？ご自愛ください";
+  } else if (nowHour < 8) {
+    clockComments = "おはようございます！今日も1日頑張りましょう！";
+  } else if (nowHour < 17) {
+    clockComments = "何してる？ぼくはいま学校、、、なのかな？";
+  } else {
+    clockComments = "今日も1日お疲れ様でした！ビール飲んで寝よ！";
+  }
+
+  timeComments.innerHTML = clockComments;
 }
 setInterval("showClock2()", 1000);
